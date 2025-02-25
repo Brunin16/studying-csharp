e6c456602cf1038635c34786ad08254170b50483
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-25 20:12:55 -0300

    initial project

---
+++ FiapOperacoes/FiapOperacoes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiapOperacoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digita teu nome rapa");
+            String read =Console.ReadLine();
+            Console.WriteLine("tu digito:" + read);
+
+
+            Console.ReadKey();
+        }
+    }
+}
